68c0aff664e0464ae162ba165c29373700e68761
Ali Gamal <ali.gamal2282003@gmail.com>
2024-05-05 17:27:15 +0300

    handal error app page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import HeroSection from "./Components/Home/HeroSection";
 import { Route, Routes } from "react-router";
-import Index from './Components/Pages/Meals/index';
+import Index from "./Components/Pages/Meals";
 import axios from "axios"
 import SingleMealPage from "./Components/Pages/Meals/SingleMealPage";
 import SavedMeals from "./Components/SavedMeals/SavedMeals";
@@ -21,16 +21,16 @@ function App() {
           position="bottom-right"
           toastOptions={{
             style: {
-              fontSize: "1.4rem",
+              fontSize: '1.4rem',
             },
           }}
         />
         <Routes>
-          <Route element={<Layouts/>}>
-            <Route path="/" element={<HeroSection/>} />
-            <Route path="meals" element={<Index/>} />
-            <Route path="meals/:Id" element={<SingleMealPage/>} />
-            <Route path="savedMeals" element={<SavedMeals/>} />
+          <Route element={<Layouts />}>
+            <Route path="/" element={<HeroSection />} />
+            <Route path="meals" element={<Index />} />
+            <Route path="meals/:Id" element={<SingleMealPage />} />
+            <Route path="savedMeals" element={<SavedMeals />} />
           </Route>
         </Routes>
       </QueryClientProvider>
